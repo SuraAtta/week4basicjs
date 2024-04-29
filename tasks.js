f5fb55f565230ae976183fbc661d537626eaf8f1
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2024-04-30 00:33:32 +0300

    fixing

---
+++ tasks.js
@@ -0,0 +1,75 @@
+// Task 1
+function checkVotingEligibility(age) {
+  if (age >= 18)
+    console.log("You are eligible for voting,")
+  else
+    console.log("You are not eligible for voting.")
+}
+
+// Task 2
+function printNumbersWithWhile() {
+  let i = 1;
+
+  while (i <= 5) {
+    console.log(i);
+    i++;
+  }
+}
+
+// Task 3
+function printEvenNumbersWithFor() {
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+      console.log(i)
+    }
+  }
+}
+
+// Task 4
+function getDayOfWeek(number) {
+  switch (number) {
+    case 1:
+      return "Sunday";
+    case 2:
+      return "Monday";
+    case 3:
+      return "Tuesday";
+    case 4:
+      return "Wednesday";
+    case 5:
+      return "Thursday";
+    case 6:
+      return "Friday";
+    case 7:
+      return "Saturday";
+    default:
+      return "Invalid day number";
+  }
+}
+
+// Task 5
+function sum(x, y) {
+  return x + y;
+}
+
+// Task 6
+const fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+function logEachFruit(fruits) {
+  fruits.forEach((fruit) => {
+    console.log(fruit);
+  });
+}
+
+
+module.exports = {
+  checkVotingEligibility,
+  printNumbersWithWhile,
+  printEvenNumbersWithFor,
+  getDayOfWeek,
+  sum,
+  logEachFruit,
+};
+const utils = require('./tasks');
+
+console.log(utils.getDayOfWeek(2))
